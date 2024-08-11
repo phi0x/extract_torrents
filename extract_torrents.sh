5bad51cd825d6d065ff7850bd629485a0e3cd841
RCG <ryangrant778@gmail.com>
2024-08-10 17:44:34 -0700

    Update extract_torrents.sh
    fix: handle null bytes and clean up script output
    
    - Updated the 'bdecode' function to handle and remove null bytes from extracted values using 'tr -d \0'.
    - Added additional debug output to ensure paths and filenames are correctly determined.
    - Ensured that directory paths are verified and created if they do not exist.
    - Included checks for the execution flag to conditionally execute file operations.
    - Separated progress status ('o') from debug statements to avoid cluttering the output.
    - Added a final message to indicate the end of script execution.
    
    These changes address the issue of null bytes causing warnings, ensure that the script correctly saves the .torrent files to the specified output directory, and improve the readability of the script's output.

--- extract_torrents.sh
+++ extract_torrents.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #--Default configuration data: modify for your installation
-SessionDir="/home/rhino/.sessions/"
-DownloadDir="/home/rhino/torrents/"
-OutputBaseDir="/home/rhino/.torrents/"
+SessionDir="/YOUR_PATH_TO_RTORRENT_SESSION_FOLDER/config/rtorrent/session/"
+DownloadDir="COPY_FROM_RTORRENT_CONFIG_FILE"
+OutputBaseDir="/PATH_TO_EXTRACT_TORRENTS_WHERE_YOU_WANT_OUTPUTTED_TORRENTS/extract_torrents/torrents/"
 #------------------USAGE HELP------------------------------------------------->
 function usage {
-	cat <<EOM
+        cat <<EOM
 Usage: $(basename "$0") [OPTION]...
 
 This script will extract infohash.torrents from the .session directory, rename
@@ -30,7 +30,7 @@ subdirectory structure than their downloaded files.
   -h          Display this help
 EOM
 
-	exit 2
+        exit 2
 }
 #------------------PARSE OPTIONS---------------------------------------------->
 # init switch flags
@@ -40,65 +40,67 @@ altname=0
 execute=0
 
 while getopts ":s:d:o:t:l:axh" optKey; do
-	case $optKey in
-		s)
-			SessionDir="$OPTARG"
+        case $optKey in
+                s)
+                        SessionDir="$OPTARG"
       if [ ! -d "$SessionDir" ];then
-      	echo "ERROR: Parameter -s path ($SessionDir) doesn't exist"
-      	exit 2
-      fi
-			;;
-		d)
-			DownloadDir="$OPTARG"
-      if [ ! -d "$DownloadDir" ];then
-      	echo "ERROR: Parameter -d path ($DownloadDir) doesn't exist"
+        echo "ERROR: Parameter -s path ($SessionDir) doesn't exist"
         exit 2
       fi
-			;;
-		o)
-			OutputBaseDir="$OPTARG"
-			;;
-		t)
-			filterTracker="$OPTARG"
+                        ;;
+                d)
+                        DownloadDir="$OPTARG"
+      if [ ! -d "$DownloadDir" ];then
+        echo "ERROR: Parameter -d path ($DownloadDir) doesn't exist"
+        exit 2
+      fi
+                        ;;
+                o)
+                        OutputBaseDir="$OPTARG"
+                        ;;
+                t)
+                        filterTracker="$OPTARG"
       echo "Will extract only .torrents for $filterTracker"
-			;;
-		l)
-			filterLabel="$OPTARG"
-			;;
+                        ;;
+                l)
+                        filterLabel="$OPTARG"
+                        ;;
 
-		a)
-			altname=1
+                a)
+                        altname=1
       echo "Will use alternative names for the .torrents"
-			;;
-		x)
-			execute=1
-			;;
-		h|*)
-			usage
-			;;
-	esac
+                        ;;
+                x)
+                        execute=1
+                        ;;
+                h|*)
+                        usage
+                        ;;
+        esac
 done
 
 shift $((OPTIND - 1))
 
 #------------------DEFINE FUNCTIONS------------------------------------------->
-# sustitute urlencoded characters like %20 by normal characters
+# substitute urlencoded characters like %20 by normal characters
 urldecode() { : "${*//+/ }"; echo -e "${_//%/\\x}"; }
+
 # extract the value of key $1 from a bencoded file $2  ej:  bdecode x-filename  bencoded.txt
 bdecode() {
-	local key="${#1}:$1"
-	local value=$(sed  -nE -e "s/^.*$key//p" $2)
-	local valueLen=$(echo $value | cut -d: -f1)
-	local valueText=$(echo "$value" | cut -d: -f2-)
+        local key="${#1}:$1"
+        local value=$(sed  -nE -e "s/^.*$key//p" $2 | tr -d '\0')
+        local valueLen=$(echo $value | cut -d: -f1)
+        local valueText=$(echo "$value" | cut -d: -f2-)
   if [ "$valueLen" = "0" ]; then
-  	echo ""
+        echo ""
   else
-  	echo "$valueText" | cut -c1-$valueLen
+        echo "$valueText" | cut -c1-$valueLen
   fi
 }
+
 # quote special metacharacters with \
 metaquote() {
-	printf %s "$1" | sed 's/[][{}()\/.^$?*+]/\\&/g'
+        printf %s "$1" | sed 's/[][{}()\/.^$?*+]/\\&/g'
 }
 
 #------------------MAIN CODE-------------------------------------------------->
@@ -111,37 +113,37 @@ OutputBaseDir="${OutputBaseDir%/}/"
 for torfile in "$SessionDir"*.torrent
 do
   rtorfile="$torfile.rtorrent"
-#	--extract tracker name (as shown in rutorrent, i.e. hdbits.org ) from hash.torrent (key=announce)
-	announce="$(bdecode announce $torfile)"
+#       --extract tracker name (as shown in rutorrent, i.e. hdbits.org ) from hash.torrent (key=announce)
+        announce="$(bdecode announce $torfile)"
   domain="$(echo $announce  | sed -e "s/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/")"
   tracker="$(echo $domain | rev | cut -d. -f-2 | rev )"
   if [ "$filterTracker" != "" ] && [ "$filterTracker" != "$tracker" ]  ; then
     printf "."
-  	continue
+        continue
   fi
 # --extract rutorrent label from hash.rtorrent (key=custom1
-	label="$(bdecode custom1 $rtorfile)"
+        label="$(bdecode custom1 $rtorfile)"
   label="$(urldecode $label)"
   if [ "$filterLabel" != "" ] && [ "$filterLabel" != "$label" ]  ; then
     printf "."
     continue
   fi
 # --extract the original xxx.torrent name from hash.rtorrent (key=x-filename, urlencoded)
-	TorrName="$(bdecode x-filename $rtorfile)"
-	TorrName=$(urldecode $TorrName)
+        TorrName="$(bdecode x-filename $rtorfile)"
+        TorrName=$(urldecode $TorrName)
 # --extract the filename (or directory if multifile) of the download from hash.torrent (key=name)
-	DownName=$(bdecode name $torfile)
+        DownName=$(bdecode name $torfile)
   DownNameQ=$(metaquote "$DownName")
 # --extract full download path from hash.rtorrent (key=directory)
-	DownPath="$(bdecode directory $rtorfile)"
-#		-- take out the last path segment when it is a multifile download to a directory
+        DownPath="$(bdecode directory $rtorfile)"
+#               -- take out the last path segment when it is a multifile download to a directory
     DownPathS=$(printf %s "$DownPath" | sed -E -e "s/$DownNameQ//g")
-#		-- take out the trailing slash to unify (sometimes is not present)
+#               -- take out the trailing slash to unify (sometimes is not present)
     DownPathS=$(printf %s "$DownPathS" | sed -e "s/\/$//g")
-#		-- replace DownloadDir with OutputBaseDir
-	DownloadDirQ=$(metaquote "$DownloadDir")
+#               -- replace DownloadDir with OutputBaseDir
+        DownloadDirQ=$(metaquote "$DownloadDir")
   OutputBaseDirQ=$(metaquote "$OutputBaseDir")
-	OutputPath=$(printf %s "$DownPathS" | sed -E -e "s/$DownloadDirQ/$OutputBaseDirQ/g")
+        OutputPath=$(printf %s "$DownPathS" | sed -E -e "s/$DownloadDirQ/$OutputBaseDirQ/g")
 # --extract hash file name
   HashName=$(echo "$torfile" | grep -o '[^/]*$' )
 
@@ -154,14 +156,17 @@ do
 
 # --use alternative name if requested or when real name not valid
   if [ $altname -eq 1 ] || [ "$TorrName" == "" ] || [ "$TorrName^^" == "$HashName^^" ] ; then
-  	TorrName="$DownName.torrent"
+        TorrName="$DownName.torrent"
   fi
 # --execute the mkdir/cp commands if -x, or just explain what will do
   if [ $execute -eq 1 ]; then
-  	printf "o"
-  	mkdir -p "$OutputPath"
-  	cp "$torfile" "$OutputPath/$TorrName"
+        printf "o"
+        mkdir -p "$OutputPath"
+        cp "$torfile" "$OutputPath/$TorrName"
   else
-  	printf "\ncp $HashName to $OutputPath/$TorrName"
+        printf "\ncp $HashName to $OutputPath/$TorrName"
   fi
 done
+
+echo
+echo "Operation completed."
